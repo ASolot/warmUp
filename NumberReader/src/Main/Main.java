3049d3519e4e18ac83c588fe8d05e1da8db4bb6f
Oodapow <petre_v_alexandru@yahoo.com>
2016-03-13 23:27:26 +0200

    GUI
    Testing

--- NumberReader/src/Main/Main.java
+++ NumberReader/src/Main/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+package Main;
 /**
  *
  * @author Petre Alexandru
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HelloWorld");
     }
     
 }
